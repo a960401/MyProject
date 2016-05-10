2dd2b4e583ea69e27ef3a7729d308bb4cf3c8d09
a960401 <a960401sogang@gmail.com>
2016-05-10 08:37:11 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email:'a960401sogang@gmail.com', password: 'lchmm516578')
+User.create(email:'now2016@naver.com', password: 'sgbiznow+')
 
 Product.create(category: '우산', total_amount: 50, lended_amount: 10)
 Product.create(category: '재무계산기', total_amount: 5, lended_amount: 2)
